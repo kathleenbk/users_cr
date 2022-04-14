4c0fa108b8ec68795b3d1fc67627b9d1809919a6
kathleenbk <katie.kava3@gmail.com>
2022-04-13 20:01:28 -0800

    intial commit

--- server.py
+++ server.py
@@ -1,54 +1,5 @@
-from flask import Flask, render_template, request, redirect, session
-
-from user import User
-
-app = Flask(__name__)
-@app.route("/")
-def index():
-    return redirect('/users')
-
-@app.route("/users")
-def users():
-    users = User.get_all()
-    return render_template("read.html", users=users)
-
-
-@app.route("/user/new")
-def new():
-    return render_template("create.html")
-
-@app.route('/user/create', methods=["POST"])
-def create_user():
-    print(request.form)
-    User.save(request.form)
-    return redirect('/users')
-
-@app.route('/user/edit/<int:id>')
-def edit(id):
-    data ={ 
-        "id":id
-    }
-    return render_template("edit.html",user=User.get_one(data))
-
-@app.route('/user/update', methods=["POST"])
-def update():
-    User.update(request.form)
-    return redirect('/users')
-
-@app.route('/user/destroy/<int:id>')
-def destroy(id):
-    data ={
-        "id":id
-    }
-    User.destroy(data)
-    return redirect('/users')
-
-@app.route('/user/show/<int:id>')
-def show(id):
-    data ={ 
-        "id":id
-    }
-    return render_template("show.html",user=User.get_one(data))
+from flask_app.controllers import users
+from flask_app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
